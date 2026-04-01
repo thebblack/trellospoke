6d101633d5f4f8c353c717ba1289ee44f70c1967
thebblack <thebblack@gmail.com>
2026-04-01 12:10:43 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,4 @@ export default defineConfig({
   server: {
     allowedHosts: true,
   },
-  build: {
-    rollupOptions: {
-      // This tells Vite: "Don't try to bundle leaflet, it exists globally"
-      external: ['leaflet'],
-      output: {
-        globals: {
-          leaflet: 'L',
-        },
-      },
-    },
-  },
 });
