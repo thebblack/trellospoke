e4581d0c5626333482454329dce72031a00c275e
thebblack <thebblack@gmail.com>
2026-04-01 14:14:52 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/trellospoke/",
   plugins: [react()],
-  server: {
-    allowedHosts: true,
+  build: {
+    outDir: "dist",
   },
 });
